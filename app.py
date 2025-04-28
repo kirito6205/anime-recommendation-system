74d11e4ba75749445e250146ef5c0ee7cdf2f7f5
kirito6205 <kirito6205@gmail.com>
2025-04-28 12:30:06 +0530

    app.py

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ def logout():
 
 if __name__ == '__main__':
     if not os.path.exists('users.db'):
+        from app import db
         db.create_all()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
