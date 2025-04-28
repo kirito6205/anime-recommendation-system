c7a22ef7b7df2e5052087ba1c7985b0c1a11c9a0
kirito6205 <kirito6205@gmail.com>
2025-04-28 20:21:58 +0530

    app.py

--- app.py
+++ app.py
@@ -82,9 +82,8 @@ def logout():
     logout_user()
     flash('You have been logged out.', 'info')
     return redirect(url_for('login'))
-
+with app.app_context():
+    db.create_all()
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
